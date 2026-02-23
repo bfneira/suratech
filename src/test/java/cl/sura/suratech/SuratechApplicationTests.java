73e422f4f731037909841f6d683efe18309f260f
Bernabé Fonseca <bfneira@gmail.com>
2026-02-22 21:35:26 -0300

    fix error junit db

--- src/test/java/cl/sura/suratech/SuratechApplicationTests.java
+++ src/test/java/cl/sura/suratech/SuratechApplicationTests.java
@@ -1,12 +1,41 @@
 package cl.sura.suratech;
 
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest(properties = {
-        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+        "spring.task.scheduling.enabled=false",
+        "app.outbox.enabled=false"
 })
 class SuratechApplicationTests {
-    @Test void contextLoads() {}
+
+    @MockBean
+    ServiceBusSenderClient quoteEventsSender;
+
+    @Container
+    static MySQLContainer<?> mysql =
+            new MySQLContainer<>("mysql:8.3")
+                    .withDatabaseName("suratech")
+                    .withUsername("root")
+                    .withPassword("root");
+
+    @DynamicPropertySource
+    static void datasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
