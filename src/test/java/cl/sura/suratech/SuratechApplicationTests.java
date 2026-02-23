d5273803fc3eef7493b21b6db302bb0c4f5f9660
Bernabé Fonseca <bfneira@gmail.com>
2026-02-22 21:17:55 -0300

    change config junit

--- src/test/java/cl/sura/suratech/SuratechApplicationTests.java
+++ src/test/java/cl/sura/suratech/SuratechApplicationTests.java
@@ -3,11 +3,10 @@ package cl.sura.suratech;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class SuratechApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+    @Test void contextLoads() {}
 }
